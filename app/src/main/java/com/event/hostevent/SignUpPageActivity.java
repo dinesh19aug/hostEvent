7fe95a21850f1ec3c4fb67345bffbccdbea4376c
dinesh19aug@gmail.com <dinesh19aug@gmail.com>
2018-05-15 21:37:44 -0400

    Added code to add a record in firebase database

--- app/src/main/java/com/event/hostevent/SignUpPageActivity.java
+++ app/src/main/java/com/event/hostevent/SignUpPageActivity.java
@@ -10,22 +10,33 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpPageActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
+
     private static String TAG = "SignUpPageActivity";
     private Button signUpButton;
 
+
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         setUpSignUp();
     }
 
@@ -43,6 +54,7 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
                         if (task.isSuccessful()) {
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            saveRecord();
 
                         } else {
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -52,6 +64,30 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
                         }
                     }
                 });
+    }
+
+    private void saveRecord() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("First_Name", "Ada");
+        user.put("Last_Name", "Lovelace");
+        user.put("Phone_Number", 1815);
+        user.put("Email_Id", "dinesh@gmail.com");
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
     }
 
 
